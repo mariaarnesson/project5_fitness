801b0632c0451d39ba40e05cfd027411ed8bcdad
mariaarnesson <maria_arnesson@outlook.com>
2023-06-27 21:14:24 +0000

    added querset and generic

--- reservation/views.py
+++ reservation/views.py
@@ -1,34 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import View
+from django.views.generic import View
 from .models import OnlineBooking
 from .forms import OnlineBookingForm
 from datetime import date
 from django.contrib import messages
+from django.utils.decorators import method_decorator
 
 
 class HomeView(View):
+
     def get(self, request):
         return render(request, 'home.html', {})
 
 
 class ReservationView(View):
+
     def get(self, request):
         return render(request, 'reservation.html')
 
 
 class MenuView(View):
+
     def get(self, request):
         return render(request, 'menu.html')
 
 
+@method_decorator(login_required, name='dispatch')
 class OnlineBookingView(View):
-    def get(self, request):
+
+    template_name = 'online_booking.html'
+    queryset = OnlineBooking.objects.filter(approved=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        total_tables = 30
+        booked_tables = OnlineBooking.objects.count()
+        available_tables = total_tables - booked_tables
         form = OnlineBookingForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'online_booking.html', context)
+        context['form'] = form
+        context['available_tables'] = available_tables
+        return context
 
     def post(self, request):
         form = OnlineBookingForm(request.POST)
@@ -52,8 +65,9 @@ class OnlineBookingView(View):
         }
         return render(request, 'online_booking.html', context)
 
-
+@method_decorator(login_required, name='dispatch')
 class MyBookingsView(View):
+
     def get(self, request):
         online_bookings = OnlineBooking.objects.filter(user=request.user)
         context = {
@@ -61,8 +75,9 @@ class MyBookingsView(View):
         }
         return render(request, 'mybookings.html', context)
 
-
+@method_decorator(login_required, name='dispatch')
 class EditBookingView(View):
+
     def get(self, request, booking_id):
         booking = get_object_or_404(OnlineBooking, id=booking_id, user=request.user)
         form = OnlineBookingForm(instance=booking)
@@ -85,8 +100,9 @@ class EditBookingView(View):
         }
         return render(request, 'edit_booking.html', context)
 
-
+@method_decorator(login_required, name='dispatch')
 class DeleteBookingView(View):
+
     def get(self, request, booking_id):
         booking = get_object_or_404(OnlineBooking, id=booking_id, user=request.user)
         context = {
