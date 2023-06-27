f238ae661ed4e48ee91aeb9ce339ffa4b4d48ee0
mariaarnesson <maria_arnesson@outlook.com>
2023-06-27 21:55:43 +0000

    test to book with oter user- success

--- reservation/views.py
+++ reservation/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import View
-from django.views.generic import View
 from .models import OnlineBooking
 from .forms import OnlineBookingForm
 from datetime import date
 from django.contrib import messages
-from django.utils.decorators import method_decorator
 
 
 class HomeView(View):
@@ -27,45 +25,62 @@ class MenuView(View):
         return render(request, 'menu.html')
 
 
-@method_decorator(login_required, name='dispatch')
+class MyBookingsView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            online_bookings = OnlineBooking.objects.filter(user=request.user)
+            context = {
+                'online_bookings': online_bookings,
+            }
+            return render(request, 'mybookings.html', context)
+        else:
+            # Handle the case when the user is not logged in
+            messages.error(request, 'You need to log in to view your bookings.')
+            return redirect('login')
+
+
 class OnlineBookingView(View):
 
-    template_name = 'online_booking.html'
-    queryset = OnlineBooking.objects.filter(approved=True)
+    total_tables = 30
+    booked_tables = OnlineBooking.objects.count()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        total_tables = 30
-        booked_tables = OnlineBooking.objects.count()
-        available_tables = total_tables - booked_tables
+    def get(self, request):
+        available_tables = self.total_tables - self.booked_tables
         form = OnlineBookingForm()
-        context['form'] = form
-        context['available_tables'] = available_tables
-        return context
-
-    def post(self, request):
-        form = OnlineBookingForm(request.POST)
-
-        if form.is_valid():
-            reservation = form.save(commit=False)
-            if reservation.date < date.today():
-                messages.error(request, 'You cannot book a table for a past date.')
-                return redirect('online_booking')
-
-            reservation.user = request.user
-            reservation.approved = False
-            reservation.save()
-            messages.success(request, 'Reservation request submitted successfully. Your booking is pending approval.')
-            return redirect('mybookings')
-        else:
-            messages.error(request, 'The table is already booked.')
-
         context = {
             'form': form,
+            'available_tables': available_tables,
         }
         return render(request, 'online_booking.html', context)
 
-@method_decorator(login_required, name='dispatch')
+    def post(self, request):
+
+        form = OnlineBookingForm(request.POST)
+
+        if request.user.is_authenticated:
+            
+            if form.is_valid():
+                reservation = form.save(commit=False)
+                if reservation.date < date.today():
+                    messages.error(request, 'You cannot book a table for a past date.')
+                    return redirect('online_booking')
+
+                reservation.user = request.user
+                reservation.approved = False
+                reservation.save()
+                messages.success(request, 'Reservation request submitted successfully. Your booking is pending approval.')
+                return redirect('mybookings')
+            else:
+                messages.error(request, 'The table is already booked.')
+        else:
+            messages.error(request, 'You need to log in to make a booking.')        
+
+            context = {
+                'form': form,
+            }
+            return render(request, 'online_booking.html', context)
+
+
 class MyBookingsView(View):
 
     def get(self, request):
@@ -75,7 +90,7 @@ class MyBookingsView(View):
         }
         return render(request, 'mybookings.html', context)
 
-@method_decorator(login_required, name='dispatch')
+
 class EditBookingView(View):
 
     def get(self, request, booking_id):
@@ -100,7 +115,7 @@ class EditBookingView(View):
         }
         return render(request, 'edit_booking.html', context)
 
-@method_decorator(login_required, name='dispatch')
+
 class DeleteBookingView(View):
 
     def get(self, request, booking_id):
