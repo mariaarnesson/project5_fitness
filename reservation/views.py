0ab305dbce51e14287370dda06791a2a269d0b03
mariaarnesson <maria_arnesson@outlook.com>
2023-06-29 08:05:27 +0000

    Added changes to lay out base.html

--- reservation/views.py
+++ reservation/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from .models import OnlineBooking
-from .forms import OnlineBookingForm, ContactForm
+from .forms import OnlineBookingForm
 from datetime import date
 from django.contrib import messages
 
